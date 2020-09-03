741451c11198d56119e660991ef5f657162a4a2d
Cynthia Santee <santeecynthia@gmail.com>
2020-09-03 01:51:04 -0700

    Add types to theme

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+// my-theme.ts
+import { DefaultTheme } from 'styled-components'
+
+const myTheme: DefaultTheme = {
+    primaryColor: '#08415c',
+    secondaryColor: '#bfdbf7',
+    tertiaryColor: '#e1e5f2',
+    fontColor: '#303030',
+    fontColorLight: 'white'
+}
+
+export { myTheme }
