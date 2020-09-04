9307cec6c36a55e36db69ea45d16c4c6855d690f
Cynthia Santee <santeecynthia@gmail.com>
2020-09-03 18:49:31 -0700

    Updates

---
+++ precache-manifest.19fe1c799b42180dc75fa7c3114321e4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72ab89f43276e2ae1e35061ebc2d094",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e74d55ea5f8b705e830",
+    "url": "/static/css/main.dacb31c4.chunk.css"
+  },
+  {
+    "revision": "4b1df1e2f46c85ee74a1",
+    "url": "/static/js/2.ce7cb97c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ce7cb97c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e74d55ea5f8b705e830",
+    "url": "/static/js/main.62528906.chunk.js"
+  },
+  {
+    "revision": "932aacb0e99177fbb297",
+    "url": "/static/js/runtime-main.d85db5c2.js"
+  },
+  {
+    "revision": "2c46a562524564cc735208f4d2165d9e",
+    "url": "/static/media/courthoops.2c46a562.png"
+  },
+  {
+    "revision": "1124b23e0ba2a41df01733786e0e5658",
+    "url": "/static/media/courthoops2.1124b23e.png"
+  },
+  {
+    "revision": "5a5db7a29498fd2aed65614153cda012",
+    "url": "/static/media/courthoops3.5a5db7a2.png"
+  },
+  {
+    "revision": "6080783b5055e670c9975584eac302b9",
+    "url": "/static/media/courthoops4.6080783b.png"
+  },
+  {
+    "revision": "1f20cc1b538a9a46e468bf606b95cf2c",
+    "url": "/static/media/cover.1f20cc1b.jpg"
+  },
+  {
+    "revision": "b40414241bb2aa81a0df325b27d7deed",
+    "url": "/static/media/mobileCover.b4041424.jpg"
+  }
+]);
